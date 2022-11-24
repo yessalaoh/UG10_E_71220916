d1727146a83c5fdc97b65cf3cb7642c5668fbf6a
yessalaoh <118967645+yessalaoh@users.noreply.github.com>
2022-11-24 16:12:04 +0700

    Add files via upload

---
+++ 2_D_71220916.py
@@ -0,0 +1,21 @@
+nama =input("masukan nama pemain pertama:")
+print("ana mendapat kartu sebanyak 12")
+
+nama =input("masukan nama pemain kedua:")
+print("ena mendapat kartu sebanyak 20")
+
+nama =input("masukan nama pemain ketiga :")
+print("una mendapat kartu sebanyak 19")
+
+print("ena menang dengan jumlah kartu sebanyak 20")
+
+
+#test case 2
+nama =input("masukan nama pemain pertama :")
+print("alya mendapat kartu sebanyak 33")
+
+nama=input("masukan nama pemain kedua :")
+print("angie mendapat kartu sebanyak 50")
+
+nama=input("masukan nama pemain ketiga :")
+print("ariel mendapat kartu sebanyak 11")
